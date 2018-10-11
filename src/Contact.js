dd9be7f3b241dbd1c1197f3f4834ce77b8d93a27
Pavel Smolin <isingwithaz@gmail.com>
2018-10-11 16:26:26 +0300

    add open contacts card handler

--- src/Contact.js
+++ src/Contact.js
@@ -2,16 +2,38 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
 class Contact extends Component {
+  state = {
+    opened: false
+  };
+
+  handleClick = () => {
+    this.setState({ opened: !this.state.opened });
+  };
+
   render() {
     const { name, email, phone } = this.props;
 
     return (
       <div className="card card-body mb-3">
-        <h4>{name}</h4>
-        <ul className="list-group">
-          <li className="list-group-item">Email: {email}</li>
-          <li className="list-group-item">Phone: {phone}</li>
-        </ul>
+        <h4>
+          {name}{' '}
+          <i
+            className="fas fa-sort-down"
+            onClick={this.handleClick}
+            style={{ cursor: 'pointer' }}
+          />
+          <i
+            className="fas fa-times"
+            onClick={this.handleClick}
+            style={{ cursor: 'pointer', float: 'right', color: 'red' }}
+          />
+        </h4>
+        {this.state.opened && (
+          <ul className="list-group">
+            <li className="list-group-item">Email: {email}</li>
+            <li className="list-group-item">Phone: {phone}</li>
+          </ul>
+        )}
       </div>
     );
   }
